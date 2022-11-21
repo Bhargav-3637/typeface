ad3ab13c90922447ba68060b11a0c20941558bef
Bhargav-3637 <60752444+Bhargav-3637@users.noreply.github.com>
2022-11-21 15:45:42 +0530

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class DecToBase{
+public static void main(String ar[]){
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+String st=Integer.toString(a,3);
+System.out.println(st);
+}
+}
